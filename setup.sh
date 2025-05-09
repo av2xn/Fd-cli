26921c2c872a0834da8b6a96a9563928bcc7c6b6
av2xn <av2xn@protonmail.com>
2025-05-09 20:33:40 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Hedef konum
+
 TARGET="/data/data/com.termux/files/usr/bin/fd"
 
-# Aynı dizindeki 'fd' dosyasını kopyala
+
 if [ ! -f ./fd ]; then
   echo "'fd' dosyası bu dizinde bulunamadı!"
   exit 1
 fi
 
-# Kopyala ve çalıştırılabilir yap
+
 cp ./fd "$TARGET"
 chmod +x "$TARGET"
 
-pkg install wget curl jq -y
+pkg install wget curl jq termux-tools -y
 
 clear
 
